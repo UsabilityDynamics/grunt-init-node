35b67b41ee23b7f594828b04dc0c9c0ed0cf7eaf
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-10-31 03:58:12 -0400

    Improved default module loader file.

--- root/name.js
+++ root/name.js
@@ -2,5 +2,5 @@
  * Run Coverage Tests / Load Core Module
  *
  */
-module.exports = process.env.APP_COVERAGE ? require( './static/lib-cov/{%= name %}.js' ) : require( './lib/{%= name %}.js' );
+module.exports = process.env.hasOwnProperty( 'APP_COVERAGE' ) ? require( './static/lib-cov/{%= name %}.js' ) : require( './lib/{%= name %}.js' );
 
