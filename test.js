73bfcbb25ce0ffd6898f408d6412d65b505e6b8a
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-06-23 09:11:57 -0400

    Automated commit. [ci skip]

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-module.exports = {
-  
+module.exports = {  
   'grunt-scaffold-module': { 
-    
-    'exposes methods': function() {
-      
-      
-      
-    } 
-  
-  }     
-  
+    'has valid project.yml file.': require( './' ).testProjectValidity,
+    'has valid project structure.': require( './' ).testStructure,
+    'has valid public methods.': require( './' ).testMethods,
+    'has valid public classess.': require( './' ).testClasses        
+  } 
 }
